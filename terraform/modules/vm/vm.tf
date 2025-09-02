f155933854474c35ac9ee239f1f9703d49dcee24
Hssah Alsharihi <168639436+HssahSaad@users.noreply.github.com>
2025-09-02 14:28:38 +0300

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -23,7 +23,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")   # local run; in pipeline you’ll inline the key
+    public_key = var.admin_ssh_public_key    
+
   }
 
   os_disk {
