1b6a26abeb3823b0c7201e62f3c973ff37a3cae2
Hssah Alsharihi <168639436+HssahSaad@users.noreply.github.com>
2025-09-02 14:30:33 +0300

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -23,7 +23,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = var.admin_ssh_public_key    
+    public_key = var.admin_ssh_public_key
 
   }
 
