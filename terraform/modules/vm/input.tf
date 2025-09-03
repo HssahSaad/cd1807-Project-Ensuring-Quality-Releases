23f8bea50a11e13a030af2bcf3aa23efc3c7e89c
Hssah Alsharihi <168639436+HssahSaad@users.noreply.github.com>
2025-09-03 09:23:52 +0300

    Update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -8,11 +8,7 @@ variable "vm_name"        { type = string }
 variable "vm_size"        { type = string }
 variable "admin_username" { type = string }
 
-# SSH public key content passed in by the caller (root module / pipeline)
-variable "admin_ssh_public_key" {
-  description = "The SSH public key text (e.g., contents of id_rsa.pub)"
-  type        = string
-}
+variable "admin_ssh_public_key" { type = string }
 
 variable "tags" {
   type    = map(string)
